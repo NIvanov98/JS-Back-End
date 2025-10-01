3fda33e9fa282081ab697998eaff1ddd09a86bb1
NIvanov98 <nikolai_ivanov1998@abv.bg>
2025-10-01 22:02:28 +0300

    Add express server, resources and setup handlebars

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # JS-Back-End
+
 SoftUni JS-Back-End Module
+
+## Development Steps
+
+- [x] Initialize Project
+- [x] Add Express Server
+- [x] Add resources
+- [x] Setup Handlebars
+- [] Render Home Page
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import handlebars from "express-handlebars";
+
+const app = express();
+
+//setup handlebars
+app.engine(
+	"hbs",
+	handlebars.engine({
+		extname: "hbs",
+	})
+);
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+//routes
+app.get("/", (req, res) => {
+	res.render("home", { layout: false });
+});
+//start server
+app.listen(5000, () =>
+	console.log("Server is listening on http://localhost:5000...")
+);
